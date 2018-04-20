936465d4f0a4043237cb138f633aa11d2ac1bf77
Ken M <kenneth.b.macneal@gmail.com>
2018-04-20 17:14:15 -0500

    las update of the night i think

---
+++ Pages/Decrees.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace church_of_the_emporer_as_messiah.Pages {
+    public class DecreesModel : PageModel {
+        public string Message {
+            get;
+            set;
+        }
+        public string Decree {
+            get;
+            set;
+        }
+        public void OnGet () {
+            Message = "Decrees";
+            Decree = "A guideline was constructed on how one can be genetically modified and still faithful";
+        }
+    }
+}
