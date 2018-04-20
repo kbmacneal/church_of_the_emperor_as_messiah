56c12f1f7db712a3dda60df7ef036ae59dbbf63a
bowmorek <kenneth.b.macneal@gmail.com>
2018-04-20 13:22:53 -0500

    update

--- Pages/Join.cshtml.cs
+++ Pages/Join.cshtml.cs
@@ -11,8 +11,21 @@ namespace church_of_the_emporer_as_messiah.Pages {
             get;
             set;
         }
+        public string Name {
+            get;
+            set;
+        }
+
+        public string Quote {
+            get;
+            set;
+        }
         public void OnGet () {
             Message = "How to Join the Church of the Emperor-as-Messiah";
+            Name = "Holy Reference, Sutra 5, Verse 87";
+            Quote = "Look at the stars! I cast you among them, to populate them, and bring humanity to the greatest reaches of the universe!";
+
+            //http://www.mariorodriguez.co/2016/03/24/create-a-quote-box-with-html-and-css.html
         }
     }
 }
