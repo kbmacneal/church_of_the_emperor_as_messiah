f004d0d73de580de36d660bde4501ded028cb3b9
bowmorek <kenneth.b.macneal@gmail.com>
2018-04-20 15:10:26 -0500

    update

--- Pages/10Blessings.cshtml.cs
+++ Pages/10Blessings.cshtml.cs
@@ -99,8 +99,8 @@ namespace church_of_the_emporer_as_messiah.Pages {
             recitation_2 = "Recitation: “We must be obedient to tradition, ceremony, courtesy and station.” Propriety is exemplified by respectful loyalty to righteous authority and cultural norms, along with unfailing intolerance for all heretics and heathens. Custom, dress, and technology all must adhere to Propriety.";
             blessing_3 = "The Third Virtue is Justice, which flows from Propriety.";
             recitation_3 = "Recitation: “We must reward those who behave rightly and punish those who do not.” Justice is exemplified by its unflinching enforcement, and we must correct our own failings before looking to those of others.";
-            blessing_4 = "The Fourth Virtue is Fortitude, which reinforces Justice. Recitation: “We must patiently endure the challenges laid upon us and follow the rightful path despite them.” Fortitude is exemplified by steadfast courage and endurance in the face of adversity, particularly in avoiding all cringing or complaint when upon holy ground or fulfilling holy duties.";
-            recitation_4 = "";
+            blessing_4 = "The Fourth Virtue is Fortitude, which reinforces Justice.";
+            recitation_4 = "Recitation: “We must patiently endure the challenges laid upon us and follow the rightful path despite them.” Fortitude is exemplified by steadfast courage and endurance in the face of adversity, particularly in avoiding all cringing or complaint when upon holy ground or fulfilling holy duties.";
             blessing_5 = "The Fifth Virtue is Wisdom, which accompanies Fortitude. ";
             recitation_5 = "Recitation: “We must strive to see the world in its truth and shape it according to God’s will.” Wisdom is exemplified by perceived the flawed world as it is, but never losing sight of what it should be.  Daily reflection upon the sacred texts and their application to our lives is essential to Wisdom.";
             blessing_6 = "The Sixth Virtue is Temperance, which flows from Wisdom. ";
