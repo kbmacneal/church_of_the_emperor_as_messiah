6e9c5f8935a9dfa210513a510a98aae95ee31a57
bowmorek <kenneth.b.macneal@gmail.com>
2018-04-20 14:15:54 -0500

    working on blessings

--- Pages/10Blessings.cshtml.cs
+++ Pages/10Blessings.cshtml.cs
@@ -11,6 +11,46 @@ namespace church_of_the_emporer_as_messiah.Pages {
             get;
             set;
         }
+        public string blessing_1 {
+            get;
+            set;
+        }
+         public string blessing_2 {
+            get;
+            set;
+        }
+         public string blessing_3 {
+            get;
+            set;
+        }
+         public string blessing_4 {
+            get;
+            set;
+        }
+         public string blessing_5 {
+            get;
+            set;
+        }
+         public string blessing_6 {
+            get;
+            set;
+        }
+         public string blessing_7 {
+            get;
+            set;
+        }
+         public string blessing_8 {
+            get;
+            set;
+        }
+         public string blessing_9 {
+            get;
+            set;
+        }
+        public string blessing_10 {
+            get;
+            set;
+        }
         public void OnGet () {
             Message = "The 10 Blessings of our Faith";
         }
