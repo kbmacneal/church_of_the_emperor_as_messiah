f879f1db4a8c1af655dd3c3f0fc95686f23f1462
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-04-21 16:16:42 -0500

    Initial work on mobile master

--- emperor3/Site.Mobile.Master.designer.cs
+++ emperor3/Site.Mobile.Master.designer.cs
@@ -13,15 +13,6 @@ namespace emperor3 {
     public partial class Site_Mobile {
         
         /// <summary>
-        /// HeadContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
