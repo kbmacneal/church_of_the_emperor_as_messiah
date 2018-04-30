5c6eb0cf4f783e9785330a87f5cc8564c09b12f0
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-04-29 21:16:13 -0500

    initial start to churchbot

---
+++ emperor3/Classes/churchbot.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using System;
+
+namespace emperor3.Classes
+{
+    public class churchbot
+    {
+        public static void Main(string[] args) => new churchbot().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        
+        }
+
+
+}
