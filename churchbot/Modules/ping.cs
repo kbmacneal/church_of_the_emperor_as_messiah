80131072165163580028f30d433502a34002b136
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-04-30 19:33:33 -0500

    added churchbot

---
+++ churchbot/Modules/ping.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace churchbot.Modules
+{
+    public class ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong!");
+        }
+    }
+}
---
+++ churchbot/Program.cs
@@ -0,0 +1,73 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace churchbot
+{
+    internal class Program
+    {
+        private static void Main(string[] args) => new Program().RunBotAsync(args[0]).GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync(string botToken)
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            //event subscriptions
+            _client.Log += Log;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPosition = 0;
+
+            if (message.HasStringPrefix("cb!", ref argPosition) || message.HasMentionPrefix(_client.CurrentUser, ref argPosition))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPosition, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
