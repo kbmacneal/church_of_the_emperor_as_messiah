c71eae0cec9af8a3b5f9f479638f7671a544e43d
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-05-01 21:43:21 -0500

    tring to get azure web bot to work

---
+++ azure_churchbot/Dialogs/RootDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Threading;
+
+namespace azure_churchbot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            //// calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            //// return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            if (activity.Text.ToString().Split('!')[0] == "start")
+            {
+                await StartBotASync(activity.Text.ToString().Split('!')[1]);
+                context.PostAsync("Bot Started");
+            }
+            else if (activity.Text.ToString().Split('!')[0] == "stop")
+            {
+                context.PostAsync("Bot Started");
+            }
+            else
+            {
+                await context.PostAsync($"Invalid request.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private Task StartBotASync(string start_code)
+        {
+            startbot bot = new startbot();
+            startbot.botToken = start_code;
+            Thread t = new Thread(bot.Main);
+            t.Start();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ azure_churchbot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="azure_churchbot.WebApiApplication" Language="C#" %>
