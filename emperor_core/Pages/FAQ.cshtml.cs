e080b1218603622fce9420f5499940fbf4f916dd
bowmorek <kenneth.b.macneal@gmail.com>
2018-05-22 12:02:21 -0500

    Getting ready for FAQ text

--- emperor_core/Pages/FAQ.cshtml.cs
+++ emperor_core/Pages/FAQ.cshtml.cs
@@ -4,15 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using church_of_the_emporer_as_messiah.Sermons;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
 
 namespace church_of_the_emporer_as_messiah.Pages
 {
     public class FAQ : PageModel
     {
-        public string FAQcontent{get;set;}
+        public string FAQtext{get;set;}
+        private IHostingEnvironment _env;
+
         public void OnGet()
         {
+            setContent();
+        }
 
+        protected void setContent()
+        {
+            var filename = "wwwroot/Documents/FAQ.md";
+
+            FAQtext = cls_markdown.ConvertMDtoTXT(filename);
+            
         }
     }
 }
