109846230210871e18ed5f2fb106411e62a27745
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-05-23 22:34:05 -0500

    added synth decree

--- emperor_core/Pages/Decrees.cshtml.cs
+++ emperor_core/Pages/Decrees.cshtml.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using church_of_the_emporer_as_messiah.Sermons;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace church_of_the_emporer_as_messiah.Pages {
     public class DecreesModel : PageModel {
-        public string Message {
-            get;
-            set;
-        }
-        public string Decree {
+        public string synth_decree_text {
             get;
             set;
         }
         public void OnGet () {
-            Message = "Decree - gene modification and the Sanctity of the Soul";
-            Decree = "";
+            setContent ();
+        }
+
+        protected void setContent () {
+            var filename = "wwwroot/Documents/Synthetic_Decree.md";
+            synth_decree_text = cls_markdown.ConvertMDtoTXT (filename);
+
         }
     }
 }
