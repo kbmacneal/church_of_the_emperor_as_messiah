95c006611967bdc1f03aa192f5e3cad9c666c168
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-05-30 15:38:06 -0500

    converted burial page to markdown

---
+++ emperor_core/Pages/Burial.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using church_of_the_emporer_as_messiah.Sermons;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+
+namespace church_of_the_emporer_as_messiah.Pages
+{
+    public class Burial : PageModel
+    {
+        public string sermonheader { get; set; }
+        public string burialtext { get; set; }
+
+        public void OnGet()
+        {
+            setContent();
+        }
+
+        protected void setContent()
+        {
+            var filename = "wwwroot/Documents/burial.md";
+            
+            burialtext = cls_markdown.ConvertMDtoTXT(filename);
+            
+        }
+    }
+}
