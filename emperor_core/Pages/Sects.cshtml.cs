81d9370ad50383e402b5d405d983b8ba7439f5ed
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-01 13:09:28 -0500

    Added Sect page, tweaking the to menu formatting

--- emperor_core/Pages/Sects.cshtml.cs
+++ emperor_core/Pages/Sects.cshtml.cs
@@ -14,6 +14,14 @@ namespace church_of_the_emporer_as_messiah.Pages {
             get;
             set;
         }
+        public string nox_text {
+            get;
+            set;
+        }
+        public string sol_text {
+            get;
+            set;
+        }
         public void OnGet () {
             setContent ();
         }
@@ -21,6 +29,12 @@ namespace church_of_the_emporer_as_messiah.Pages {
         protected void setContent () {
             var filename = "wwwroot/Sects/Koschei.md";
             koschei_text = cls_markdown.ConvertMDtoTXT (filename);
+
+            filename = "wwwroot/Sects/NoxFide.md";
+            nox_text = cls_markdown.ConvertMDtoTXT (filename);
+
+            filename = "wwwroot/Sects/SoulExSol.md";
+            sol_text = cls_markdown.ConvertMDtoTXT (filename);
 
         }
     }
