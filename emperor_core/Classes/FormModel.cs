f181235f6cc75c24927829478aa8176f3b59381d
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-18 15:52:36 -0500

    all that's left is validation

---
+++ emperor_core/Classes/FormModel.cs
@@ -0,0 +1,12 @@
+
+namespace church_of_the_emporer_as_messiah.Models
+{
+    public class FormData
+    {
+        public string TextBoxStringData { get; set; }
+
+        public int TextBoxIPData { get; set; }
+
+        public bool CheckboxData { get; set; }
+    }
+}
--- emperor_core/Pages/GDPR.cshtml.cs
+++ emperor_core/Pages/GDPR.cshtml.cs
@@ -7,24 +7,54 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using church_of_the_emporer_as_messiah.Sermons;
 using System.Web;
 using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace church_of_the_emporer_as_messiah.Pages
 {
     public class GDPRModel : PageModel
     {
-         public string TextBoxStringData { get; set; }
+        public string TextBoxStringData { get; set; }
 
-        public int TextBoxIntData { get; set; }
+        public string TextBoxIPData { get; set; }
 
         public bool CheckboxData { get; set; }
-        
-        public void OnGet()
+
+        public string AlertHTML {get; set;}
+
+        public class contact
         {
-            
+            public string email {get;set;}
+            public string IP {get;set;}
+            public Boolean acceptance {get;set;}
         }
 
-        public void b_submit_Click(object sender, EventArgs e){
+        public void OnGet()
+        {
+            AlertHTML = "";
+        }
 
+        [HttpPost]
+        public ActionResult Create(contact contact)
+        {
+            if(CheckboxData)
+            {
+                contact.email = TextBoxStringData;
+                contact.IP = TextBoxIPData;
+                contact.acceptance = CheckboxData;
+                AlertHTML = "";
+
+                string serialized = JsonConvert.SerializeObject(contact);
+
+                System.IO.File.WriteAllText(System.IO.Path.Combine("wwwroot/GDPR", contact.email),serialized);
+
+                AlertHTML = "<div class=\"alert alert-success\">You have successfully submitted your request. Please allow five business days for the completion of your request</div>";
+            }
+            else{
+                AlertHTML = "<div id=\"dv_alert\" class=\"alert alert-warning\"><h4 class=\"alert-heading\">Warning!</h4><p class=\"mb-0\">You must acknowledge your age and acceptance before continuing</p></div>";                
+            }
+            return Redirect("");
         }
     }
 }
