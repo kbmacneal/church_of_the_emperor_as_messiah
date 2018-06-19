ead20c29dc182a42d3a1128f146f7da6d05aad4b
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-18 20:54:00 -0500

    moving over to controller architecture

--- emperor_core/Pages/GDPR.cshtml.cs
+++ emperor_core/Pages/GDPR.cshtml.cs
@@ -19,32 +19,9 @@ namespace church_of_the_emporer_as_messiah.Pages
         public string EmailData{get;set;}
         public string IPData {get;set;}
         public Boolean CheckboxData {get;set;}
-
-        public class submission
-        {
-            public string email {get;set;}
-            public string IP {get;set;}
-            public Boolean accepted {get;set;}
-        }
-
         public void OnGet()
         {
             
-        }
-        
-        public void OnPost()
-        {
-            submission sub = new submission();
-
-            sub.email = EmailData;
-            sub.IP = IPData;
-            sub.accepted = CheckboxData;
-
-            string serialized = JsonConvert.SerializeObject(sub);
-
-            System.IO.File.WriteAllText(System.IO.Path.Combine("wwwroot/GDPR", sub.email), serialized);
-
-
         }
         
     }
