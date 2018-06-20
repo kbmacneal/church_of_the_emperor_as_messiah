22fe6359980e3392b592c298425864b0dcf6a486
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-19 19:48:54 -0500

    adding license and final cleanup

---
+++ emperor_mvc/Controllers/SupportController.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using emperor_mvc.Classes;
+using emperor_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace emperor_mvc.Controllers {
+    public class SupportController : Controller {
+
+        [HttpGet]
+        public ActionResult Index () {
+
+            return View ();
+        }
+    }
+}
