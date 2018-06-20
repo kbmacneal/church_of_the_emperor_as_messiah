d3f268bccb7d51649322581e646d19cc0e283ec6
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-20 08:35:22 -0500

    minor layout changes and decreasing number of controllers

--- emperor_mvc/Controllers/ContentController.cs
+++ emperor_mvc/Controllers/ContentController.cs
@@ -15,7 +15,38 @@ namespace emperor_mvc.Controllers
             return View();
         }
 
-        public IActionResult Reading()
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Blessings () {
+
+            return View ();
+        }
+
+        [HttpGet]
+        public ActionResult FromTheDesk () {
+
+            return View ();
+        }
+
+        [HttpGet]
+        public ActionResult Join () {
+
+            return View ();
+        }
+
+        [HttpGet]
+        public ActionResult Support () {
+
+            return View ();
+        }
+
+        [HttpGet]
+        public IActionResult Templates()
         {
             return View();
         }
