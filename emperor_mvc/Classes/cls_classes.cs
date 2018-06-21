23973f66ad143ff945beb086a57bfba80a925700
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-21 15:02:11 -0500

    getting rid of old data

--- emperor_mvc/Classes/cls_classes.cs
+++ emperor_mvc/Classes/cls_classes.cs
@@ -9,24 +9,4 @@ namespace emperor_mvc.Classes
         public Boolean CheckboxData { get; set; }
 
     }
-
-    public class user
-    {
-        public int id { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string salt_text { get; set; }
-        public int confirm_number{get;set;}
-
-        public Boolean confirmed {get;set;} = false;
-        public user()
-        {
-            Random rng = new Random();
-            this.confirm_number = rng.Next(0,1000);
-        }
-    }
-
-   
-
-
 }
