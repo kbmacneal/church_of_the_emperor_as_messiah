b2da5a6720823a9ef889e6d89e885320bec6f16f
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-25 09:35:07 -0500

    add charity announcement

---
+++ emperor_mvc/Controllers/AnnouncementsController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using emperor_mvc.Classes;
+using emperor_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace emperor_mvc.Controllers {
+    public class AnnouncementsController : Controller {
+
+       [HttpGet]
+        public IActionResult Charity()
+        {
+            return View();
+        }
+        
+        
+    }
+}
