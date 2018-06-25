038de6cf57df823e7bf2d83e2e54b9c06644cf13
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-06-25 13:22:54 -0500

    fixing mobile formatting

--- emperor_mvc/Models/PDFModel.cs
+++ emperor_mvc/Models/PDFModel.cs
@@ -14,10 +14,13 @@ namespace emperor_mvc.Models {
             set;
         }
         public string html{get;set;}
+        public string link_html{get;set;}
         public void set_html(){
             // this.html = "<iframe class=\"mx-auto d-block\" src=\"/Documents/ninth_virtue_charity.pdf\" width=\"100%\" height=\"100%\"> This browser does not support PDFs. Please download the PDF to view it: <a href=" + this.link + ">Download PDF</a></iframe>";
 
-            this.html = "<object class=\"mx-auto d-block w-100\" data=" + this.link + " type=\"application/pdf\"><embed src=" + this.link + " type=application/pdf />    </object>";
+            this.html = "<object class=\"mx-auto d-block w-100\" data=" + this.link + " type=\"application/pdf\"><embed src=" + this.link + " type=application/pdf />    </object></br>";
+
+            this.link_html= "<a href=\"" + this.link + "\" target=\"_blank\">Link to PDF</a>";
         }
     }
 }
