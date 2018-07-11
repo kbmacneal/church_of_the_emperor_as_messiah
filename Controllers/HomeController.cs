3f05f62b53de63127a310f3b5b069cd7e70b3436
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-07-11 18:57:04 -0500

    remove test page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,10 +26,5 @@ namespace emperor_mvc.Controllers
         {
             return View();
         }
-
-        public IActionResult test()
-        {
-            return View();
-        }
     }
 }
