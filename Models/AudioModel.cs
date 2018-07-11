670fdc6fa6812e29e5a89c2f603f826022063263
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-07-10 20:31:31 -0500

    centering audio player and cleanup

--- Models/AudioModel.cs
+++ Models/AudioModel.cs
@@ -21,7 +21,7 @@ namespace emperor_mvc.Models
         {
             
 
-            this.html = "<audio controls=\"controls\" autoPlay=\"autoPlay\"><source src=\"" + this.path + "\" type=\"audio/mp3\" /></audio>";
+            this.html = "<audio class=\"mx-auto d-block\" controls=\"controls\" autoPlay=\"autoPlay\"><source src=\"" + this.path + "\" type=\"audio/mp3\" /></audio>";
 
             this.link_html = "<a class=\"mx-auto d-block\" href=\"" + this.path + "\" target=\"_blank\">Link to Audio File</a>";
         }
