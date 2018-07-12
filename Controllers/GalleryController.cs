29f603061154f2c34d40e25943715cfdcfa9f7f4
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-07-12 14:49:23 -0500

    added art gallery

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using emperor_mvc.Models;
+using emperor_mvc.Classes;
+using System.IO;
+
+namespace emperor_mvc.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
