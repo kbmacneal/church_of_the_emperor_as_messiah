cd71bd4e96dd89c2956d37bd1895f8862010484c
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-07-27 18:36:55 -0500

    adding Denunciation page getting ready for release later

---
+++ Controllers/DenunciationController.cs
@@ -0,0 +1,27 @@
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using emperor_mvc.Classes;
+using emperor_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace emperor_mvc.Controllers {
+    public class DenunciationController : Controller {
+
+        [HttpGet]
+        public ActionResult Index () {
+
+            return View ();
+        }
+
+        public IActionResult Justice(PDFModel model)
+        {
+            model.link = "https://highchurch.space/Documents/Denunciation/Justice.pdf";
+            model.set_html();
+            return View("Index", model);
+        }
+    }
+}
---
+++ Models/DenunciationModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace emperor_mvc.Models {
+    public class Denunciationmodel {
+        public string link {
+            get;
+            set;
+        }
+        public string html{get;set;}
+        public string link_html{get;set;}
+        public void set_html(){
+            // this.html = "<iframe class=\"mx-auto d-block\" src=\"/Documents/ninth_virtue_charity.pdf\" width=\"100%\" height=\"100%\"> This browser does not support PDFs. Please download the PDF to view it: <a href=" + this.link + ">Download PDF</a></iframe>";
+
+            this.html = "<object class=\"mx-auto d-block w-100\" data=" + this.link + " type=\"application/pdf\"><embed src=" + this.link + " type=application/pdf />    </object></br>";
+
+            this.link_html= "<a class=\"mx-auto d-block\" href=\"" + this.link + "\" target=\"_blank\">Link to PDF</a>";
+        }
+    }
+}
