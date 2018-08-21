28c4e1994a20fcfc0c57d82212f18d861efa1377
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-08-21 10:37:42 -0500

    closing cycle

--- Classes/Controllers/HomeController.cs
+++ Classes/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace emperor_mvc.Controllers
     public class HomeController : Controller
     {
         private Dictionary<int,string> Virtues = new Dictionary<int,string>(){
-            {0,""},
+            {0,"Hope - \"We must never despair, no matter how dark the hour, as God shines their light upon us.\" Hope is exemplified by the conquest of despair, symbolized most prominently by the restriction of mourning to a designated period, as well as the teaching of the Virtues to the ignorant."},
             {1,"Faith - \"Faith above all. We must trust God and their chosen Emperox to guide us.” Faith is exemplified by daily prayer, regular attendance of Church ceremonies, dutiful tithing, and pilgrimage to holy sites."},
             {2,"Propriety - \"We must be obedient to tradition, ceremony, courtesy and station.\" Propriety is exemplified by respectful loyalty to righteous authority and cultural norms, along with unfailing intolerance for all heretics and heathens. Custom, dress, and technology all must adhere to Propriety."},
             {3,"Justice - \"We must reward those who behave rightly and punish those who do not.\" Justice is exemplified by its unflinching enforcement, and we must correct our own failings before looking to those of others."},
@@ -22,8 +22,7 @@ namespace emperor_mvc.Controllers
             {6,"Temperance - \"We must show prudent moderation and diligent control over our desires.\" Temperance is exemplified by self-restraint in all facets, particularly regular fasting and avoidance of intoxicants."},
             {7,"Diligence - \"We must be ever persistent and expend all effort and attention in keeping ourselves and others to the true path.\" Diligence is exemplified by constant, tireless vigilance against temptation and treachery in all aspects of life."},
             {8,"Charity - \"We must show compassion to those worthy of God’s mercy.\" Charity is exemplified by philanthropic acts and outreach to faithful sufferers."},
-            {9,"Integrity - \"We must honor our oaths and uphold the truth.\" Integrity is exemplified by unfailing honesty and the exposure of deviants, as well as regular confession of our failings."},
-            {10,"Hope - \"We must never despair, no matter how dark the hour, as God shines their light upon us.\" Hope is exemplified by the conquest of despair, symbolized most prominently by the restriction of mourning to a designated period, as well as the teaching of the Virtues to the ignorant."}
+            {9,"Integrity - \"We must honor our oaths and uphold the truth.\" Integrity is exemplified by unfailing honesty and the exposure of deviants, as well as regular confession of our failings."}
         };
         public IActionResult Index(HomeModel model)
         {
