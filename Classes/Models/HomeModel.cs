7d7cb8118f1578e21d2a89a69e51d2ed256189f5
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-08-21 10:27:52 -0500

    added the virtue of the day

---
+++ Classes/Models/HomeModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace emperor_mvc.Models
+{
+    public class HomeModel
+    {
+        public string VirtueText{get;set;}
+        
+    }
+}
