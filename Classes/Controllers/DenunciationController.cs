fe6bfe4774ea8da3cec227a0863ff23aa8448354
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-08-24 09:47:04 -0500

    propriety going live

--- Classes/Controllers/DenunciationController.cs
+++ Classes/Controllers/DenunciationController.cs
@@ -34,7 +34,7 @@ namespace emperor_mvc.Controllers {
         {
             model.link = "https://highchurch.space/Documents/Denunciation/Propriety.pdf";
             model.set_html();
-            return View("UnderConstruction", model);
+            return View("Denunciation", model);
         }
         public IActionResult Wisdom(DenunciationModel model)
         {
