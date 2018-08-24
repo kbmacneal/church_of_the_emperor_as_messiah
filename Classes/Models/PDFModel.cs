5703ae6d906365a5742ecb780cfc545e18e923ec
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-08-24 11:40:04 -0500

    switching to the more attractive gview pdf displayer

--- Classes/Models/PDFModel.cs
+++ Classes/Models/PDFModel.cs
@@ -18,7 +18,9 @@ namespace emperor_mvc.Models {
         public void set_html(){
             // this.html = "<iframe class=\"mx-auto d-block\" src=\"/Documents/ninth_virtue_charity.pdf\" width=\"100%\" height=\"100%\"> This browser does not support PDFs. Please download the PDF to view it: <a href=" + this.link + ">Download PDF</a></iframe>";
 
-            this.html = "<object class=\"mx-auto d-block w-100\" data=" + this.link + " type=\"application/pdf\"><embed src=" + this.link + " type=application/pdf />    </object></br>";
+            // this.html = "<object class=\"mx-auto d-block w-100\" data=" + this.link + " type=\"application/pdf\"><embed src=" + this.link + " type=application/pdf />    </object></br>";
+
+            this.html = "<iframe src=\"http://docs.google.com/gview?url=" + this.link + "&embedded=true\" class=\"mx-auto d-block w-100\" frameborder=\"0\"></iframe>";
 
             this.link_html= "<a class=\"mx-auto d-block\" href=\"" + this.link + "\" target=\"_blank\">Link to PDF</a>";
         }
