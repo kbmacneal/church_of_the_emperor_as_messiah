8e7deca6577df607e50bfa85fb1a11855bff5bcf
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-10-23 10:59:08 -0500

    adding definitive environment indicator for when it fails to auto display

--- Startup.cs
+++ Startup.cs
@@ -41,6 +41,7 @@ namespace emperor_mvc {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public static void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            System.Console.WriteLine("Launching with environment: " + env.EnvironmentName);
             if (env.IsDevelopment ()) {
                 app.UseDeveloperExceptionPage ();
             } else {
