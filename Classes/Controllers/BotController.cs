64141fd7d43c443ac5210daa77726901acdf5383
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-11-20 16:59:15 -0600

    adding dynamic commands list

--- Classes/Controllers/BotController.cs
+++ Classes/Controllers/BotController.cs
@@ -8,7 +8,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace emperor_mvc.Controllers {
     public class BotController : Controller {
+        public class command_help
+    {
+        public string name {get;set;}
+        public string summary {get;set;}
+        public bool admin_required {get;set;}
+    }
         public IActionResult Index () {
+
+            var path = "commands.json";
+            var content = System.IO.File.ReadAllText(path);
+            List<command_help> commands = Newtonsoft.Json.JsonConvert.DeserializeObject<List<command_help>>(content);
+
+            ViewBag.commands = commands;
+
             return View ();
         }
     }
