887288639dfd02b337e2c7e333bf0bf1e940f7d0
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-04-16 14:24:48 -0500

    fixing dual port

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace emperor_mvc {
         }
 
         public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
-            WebHost.CreateDefaultBuilder (args)
+            WebHost.CreateDefaultBuilder (args).UseUrls("http://localhost:5000")
             .UseStartup<Startup> ();
     }
 }
