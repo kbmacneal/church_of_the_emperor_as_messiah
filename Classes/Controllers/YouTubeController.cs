80edab9eaea9e6a8b2ecbbcee66b5eef43d3b0ec
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-05-28 09:48:52 -0500

    updating link

--- Classes/Controllers/YouTubeController.cs
+++ Classes/Controllers/YouTubeController.cs
@@ -14,8 +14,8 @@ namespace emperor_mvc.Controllers
     {
         public IActionResult Revelation(YouTubeModel model)
         {
-            model.link = "https://youtu.be/P5m567jUWrA";
-            model.embed = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/P5m567jUWrA\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+            model.link = "https://youtu.be/P5m567jUWrA1";
+            model.embed = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/P5m567jUWrA1\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
             return View("Index", model);
         }
     }
