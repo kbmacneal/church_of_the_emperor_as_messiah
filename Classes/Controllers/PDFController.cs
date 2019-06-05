edc58ce641d319f3e0b6651a1d0724fe2ffea2fc
Ken MacNeal <kenneth.b.macneal@gmail.com>
2019-06-05 08:34:30 -0500

    adding latest sermon

--- Classes/Controllers/PDFController.cs
+++ Classes/Controllers/PDFController.cs
@@ -326,5 +326,12 @@ namespace emperor_mvc.Controllers
             model.set_html();
             return View("Index", model);
         }
+
+        public IActionResult BadApples(PDFModel model)
+        {
+            model.link = "https://highchurch.space/Documents/BadApples.pdf";
+            model.set_html();
+            return View("Index", model);
+        }
     }
 }
